35ccfba9abd029ebcbbf24865d8f65d380bf4d2c
Trần Huỳnh Vũ <tranhuynhvu2k4@gmail.com>
2025-11-18 22:07:16 +0700

    Update model start up

---
+++ VietStart.API/Migrations/20251118150602_updateStartup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VietStart.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateStartup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Traction",
+                table: "StartUps",
+                newName: "Plan");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Plan",
+                table: "StartUps",
+                newName: "Traction");
+        }
+    }
+}
