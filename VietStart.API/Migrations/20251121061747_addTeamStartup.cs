59ab3d71f5951acdbd8afa93156ab6c0d2590b36
Trần Huỳnh Vũ <tranhuynhvu2k4@gmail.com>
2025-11-21 13:18:17 +0700

    Add migration

---
+++ VietStart.API/Migrations/20251121061747_addTeamStartup.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VietStart.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTeamStartup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Positions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Positions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TeamStartUps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartUpId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PositionId = table.Column<int>(type: "int", nullable: false),
+                    Experience = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Motivation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamStartUps", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TeamStartUps_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TeamStartUps_Positions_PositionId",
+                        column: x => x.PositionId,
+                        principalTable: "Positions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TeamStartUps_StartUps_StartUpId",
+                        column: x => x.StartUpId,
+                        principalTable: "StartUps",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeamStartUps_PositionId",
+                table: "TeamStartUps",
+                column: "PositionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeamStartUps_StartUpId",
+                table: "TeamStartUps",
+                column: "StartUpId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeamStartUps_UserId",
+                table: "TeamStartUps",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TeamStartUps");
+
+            migrationBuilder.DropTable(
+                name: "Positions");
+        }
+    }
+}
